1b4458740ca259ed654232efa6f2c88094163640
Sunny <SunnyBingoMe@gmail.com>
2013-11-13 20:04:46 +0100

    title ok

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 <html>
 
 <head>
+    <title>Webster 配时示例</title>
     <meta charset='utf-8'>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=8">
+    <link href="http://sunnyboy.me/photos/_.jpg" id="favicon" rel="shortcut icon"> 
 </head>
 
 <body>
-<a href='https://github.com/SunnyBingoMe/webster'>点击这里</a>查看源代码.
+<a target='_blank' href='https://github.com/SunnyBingoMe/webster'>点击这里</a>查看源代码.
 </body>
 
 </html>
